17f38f8df54530230936157dd513ea97de47b563
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-19 11:26:29 -0500

    Your changing file core/src/main/java/org/apache/accumulo/core/constraints/NoDeleteConstraint.java

--- core/src/main/java/org/apache/accumulo/core/constraints/NoDeleteConstraint.java
+++ core/src/main/java/org/apache/accumulo/core/constraints/NoDeleteConstraint.java
@@ -1,21 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 package org.apache.accumulo.core.constraints;
 
 import java.util.Collections;
@@ -38,23 +21,28 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 public class NoDeleteConstraint extends org.apache.accumulo.core.data.constraints.NoDeleteConstraint
     implements Constraint {
 
+  private static final short DELETE_VIOLATION_CODE = 1;
+  private static final String DELETE_VIOLATION_MESSAGE = "Deletes are not allowed";
+
   @Override
   public String getViolationDescription(short violationCode) {
-    if (violationCode == 1) {
-      return "Deletes are not allowed";
-    }
-    return null;
+    return violationCode == DELETE_VIOLATION_CODE ? DELETE_VIOLATION_MESSAGE : null;
   }
 
   @Override
   public List<Short> check(Constraint.Environment env, Mutation mutation) {
-    List<ColumnUpdate> updates = mutation.getUpdates();
-    for (ColumnUpdate update : updates) {
-      if (update.isDeleted()) {
-        return Collections.singletonList((short) 1);
-      }
+    if (containsDelete(mutation)) {
+      return Collections.singletonList(DELETE_VIOLATION_CODE);
     }
     return null;
   }
 
+  private boolean containsDelete(Mutation mutation) {
+    for (ColumnUpdate update : mutation.getUpdates()) {
+      if (update.isDeleted()) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
