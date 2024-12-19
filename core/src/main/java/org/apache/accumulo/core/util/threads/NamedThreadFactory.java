e4fa2e7fdeff7143ec8738bb1966fba0cf77395d
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-19 13:39:47 -0500

    Your changing file core/src/main/java/org/apache/accumulo/core/util/threads/NamedThreadFactory.java

--- core/src/main/java/org/apache/accumulo/core/util/threads/NamedThreadFactory.java
+++ core/src/main/java/org/apache/accumulo/core/util/threads/NamedThreadFactory.java
@@ -1,21 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 package org.apache.accumulo.core.util.threads;
 
 import java.lang.Thread.UncaughtExceptionHandler;
@@ -47,14 +30,16 @@ class NamedThreadFactory implements ThreadFactory {
 
   @Override
   public Thread newThread(Runnable r) {
-    String threadName = null;
+    String threadName = generateThreadName(r);
+    return Threads.createThread(threadName, priority, r, handler);
+  }
+
+  private String generateThreadName(Runnable r) {
     if (r instanceof NamedRunnable) {
       NamedRunnable nr = (NamedRunnable) r;
-      threadName = String.format(FORMAT, name, nr.getName(), threadNum.getAndIncrement());
+      return String.format(FORMAT, name, nr.getName(), threadNum.getAndIncrement());
     } else {
-      threadName =
-          String.format(FORMAT, name, r.getClass().getSimpleName(), threadNum.getAndIncrement());
+      return String.format(FORMAT, name, r.getClass().getSimpleName(), threadNum.getAndIncrement());
     }
-    return Threads.createThread(threadName, priority, r, handler);
   }
 }
