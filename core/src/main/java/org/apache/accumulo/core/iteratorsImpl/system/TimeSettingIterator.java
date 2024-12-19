3cc2954c1efda341461a6c3a57f77aa94a7c1ec2
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-19 10:34:53 -0500

    Your changing file core/src/main/java/org/apache/accumulo/core/iteratorsImpl/system/TimeSettingIterator.java

--- core/src/main/java/org/apache/accumulo/core/iteratorsImpl/system/TimeSettingIterator.java
+++ core/src/main/java/org/apache/accumulo/core/iteratorsImpl/system/TimeSettingIterator.java
@@ -16,6 +16,25 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.accumulo.core.iteratorsImpl.system;
 
 import java.io.IOException;
@@ -62,7 +81,10 @@ public class TimeSettingIterator implements InterruptibleIterator {
   @Override
   public void init(SortedKeyValueIterator<Key,Value> source, Map<String,String> options,
       IteratorEnvironment env) throws IOException {
-
+    if (source == null) {
+      throw new IllegalArgumentException("Source iterator must not be null");
+    }
+    this.range = new Range();
   }
 
   @Override
@@ -78,19 +100,26 @@ public class TimeSettingIterator implements InterruptibleIterator {
   @Override
   public void seek(Range range, Collection<ByteSequence> columnFamilies, boolean inclusive)
       throws IOException {
+    this.range = range;
+    seekAndAdjustRange(columnFamilies, inclusive);
+  }
+
+  private void seekAndAdjustRange(Collection<ByteSequence> columnFamilies, boolean inclusive)
+      throws IOException {
     Range seekRange = IteratorUtil.maximizeStartKeyTimeStamp(range);
     seekRange = IteratorUtil.minimizeEndKeyTimeStamp(seekRange);
     source.seek(seekRange, columnFamilies, inclusive);
-    this.range = range;
+    adjustToValidTop();
+  }
+
+  private void adjustToValidTop() throws IOException {
     while (hasTop() && range.beforeStartKey(getTopKey())) {
       next();
     }
-
   }
 
   @Override
   public Value getTopValue() {
     return source.getTopValue();
   }
-
 }
