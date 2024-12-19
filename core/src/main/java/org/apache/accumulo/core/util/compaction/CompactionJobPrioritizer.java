d84ed18d2c002bf9f86ce796e74d3243d65af4bd
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-19 13:14:40 -0500

    Your changing file core/src/main/java/org/apache/accumulo/core/util/compaction/CompactionJobPrioritizer.java

--- core/src/main/java/org/apache/accumulo/core/util/compaction/CompactionJobPrioritizer.java
+++ core/src/main/java/org/apache/accumulo/core/util/compaction/CompactionJobPrioritizer.java
@@ -1,3 +1,4 @@
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -11,8 +12,8 @@
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
@@ -47,42 +48,22 @@ public class CompactionJobPrioritizer {
   private static final Map<Pair<NamespaceId,CompactionKind>,
       Range<Short>> ACCUMULO_NAMESPACE_RANGES = new HashMap<>();
 
-  // Create ranges of possible priority values where each range has
-  // 2000 possible values. Priority order is:
-  // root table user initiated
-  // root table system initiated
-  // metadata table user initiated
-  // metadata table system initiated
-  // other tables in accumulo namespace user initiated
-  // other tables in accumulo namespace system initiated
-  // user tables that have more files that configured system initiated
-  // user tables user initiated
-  // user tables system initiated
   static final Range<Short> ROOT_TABLE_USER = Range.of((short) 30768, (short) 32767);
   static final Range<Short> ROOT_TABLE_SYSTEM = Range.of((short) 28768, (short) 30767);
-
   static final Range<Short> METADATA_TABLE_USER = Range.of((short) 26768, (short) 28767);
   static final Range<Short> METADATA_TABLE_SYSTEM = Range.of((short) 24768, (short) 26767);
-
   static final Range<Short> SYSTEM_NS_USER = Range.of((short) 22768, (short) 24767);
   static final Range<Short> SYSTEM_NS_SYSTEM = Range.of((short) 20768, (short) 22767);
-
   static final Range<Short> TABLE_OVER_SIZE = Range.of((short) 18768, (short) 20767);
-
   static final Range<Short> USER_TABLE_USER = Range.of((short) 1, (short) 18767);
   static final Range<Short> USER_TABLE_SYSTEM = Range.of((short) -32768, (short) 0);
 
   static {
-    // root table
     SYSTEM_TABLE_RANGES.put(new Pair<>(RootTable.ID, CompactionKind.USER), ROOT_TABLE_USER);
     SYSTEM_TABLE_RANGES.put(new Pair<>(RootTable.ID, CompactionKind.SYSTEM), ROOT_TABLE_SYSTEM);
-
-    // metadata table
     SYSTEM_TABLE_RANGES.put(new Pair<>(MetadataTable.ID, CompactionKind.USER), METADATA_TABLE_USER);
     SYSTEM_TABLE_RANGES.put(new Pair<>(MetadataTable.ID, CompactionKind.SYSTEM),
         METADATA_TABLE_SYSTEM);
-
-    // metadata table
     ACCUMULO_NAMESPACE_RANGES.put(new Pair<>(Namespace.ACCUMULO.id(), CompactionKind.USER),
         SYSTEM_NS_USER);
     ACCUMULO_NAMESPACE_RANGES.put(new Pair<>(Namespace.ACCUMULO.id(), CompactionKind.SYSTEM),
@@ -93,62 +74,68 @@ public class CompactionJobPrioritizer {
       final CompactionKind kind, final int totalFiles, final int compactingFiles,
       final int maxFilesPerTablet) {
 
+    validateInputs(nsId, tableId, totalFiles, compactingFiles);
+    CompactionKind calculationKind = determineCalculationKind(kind);
+    Range<Short> range =
+        determineRange(nsId, tableId, calculationKind, totalFiles, maxFilesPerTablet);
+    Function<Range<Short>,Short> func =
+        selectPriorityFunction(totalFiles, compactingFiles, maxFilesPerTablet, calculationKind);
+
+    return func.apply(range);
+  }
+
+  private static void validateInputs(NamespaceId nsId, TableId tableId, int totalFiles,
+      int compactingFiles) {
     Objects.requireNonNull(nsId, "nsId cannot be null");
     Objects.requireNonNull(tableId, "tableId cannot be null");
     Preconditions.checkArgument(totalFiles >= 0, "totalFiles is negative %s", totalFiles);
     Preconditions.checkArgument(compactingFiles >= 0, "compactingFiles is negative %s",
         compactingFiles);
+  }
 
-    final Function<Range<Short>,Short> normalPriorityFunction = new Function<>() {
-      @Override
-      public Short apply(Range<Short> f) {
-        return (short) Math.min(f.getMaximum(), f.getMinimum() + totalFiles + compactingFiles);
-      }
-    };
-
-    final Function<Range<Short>,Short> tabletOverSizeFunction = new Function<>() {
-      @Override
-      public Short apply(Range<Short> f) {
-        return (short) Math.min(f.getMaximum(),
-            f.getMinimum() + compactingFiles + (totalFiles - maxFilesPerTablet));
-      }
-    };
-
-    // Handle the case of a CHOP compaction. For the purposes of determining
-    // a priority, treat them as a USER compaction.
-    CompactionKind calculationKind = kind;
+  private static CompactionKind determineCalculationKind(CompactionKind kind) {
     if (kind == CompactionKind.CHOP) {
-      calculationKind = CompactionKind.USER;
+      return CompactionKind.USER;
     } else if (kind == CompactionKind.SELECTOR) {
-      calculationKind = CompactionKind.SYSTEM;
+      return CompactionKind.SYSTEM;
     }
+    return kind;
+  }
 
-    Range<Short> range = null;
-    Function<Range<Short>,Short> func = normalPriorityFunction;
-    if (Namespace.ACCUMULO.id() == nsId) {
-      // Handle system tables
-      range = SYSTEM_TABLE_RANGES.get(new Pair<>(tableId, calculationKind));
+  private static Range<Short> determineRange(NamespaceId nsId, TableId tableId,
+      CompactionKind calculationKind, int totalFiles, int maxFilesPerTablet) {
+    if (Namespace.ACCUMULO.id().equals(nsId)) {
+      Range<Short> range = SYSTEM_TABLE_RANGES.get(new Pair<>(tableId, calculationKind));
       if (range == null) {
         range = ACCUMULO_NAMESPACE_RANGES.get(new Pair<>(nsId, calculationKind));
       }
-    } else {
-      // Handle user tables
-      if (totalFiles > maxFilesPerTablet && calculationKind == CompactionKind.SYSTEM) {
-        range = TABLE_OVER_SIZE;
-        func = tabletOverSizeFunction;
-      } else if (calculationKind == CompactionKind.SYSTEM) {
-        range = USER_TABLE_SYSTEM;
-      } else {
-        range = USER_TABLE_USER;
+      if (range != null) {
+        return range;
       }
+    } else {
+      return calculateUserTableRange(calculationKind, totalFiles, maxFilesPerTablet);
     }
-
-    if (range == null) {
-      throw new IllegalStateException(
-          "Error calculating compaction priority for table: " + tableId);
-    }
-    return func.apply(range);
-
+    throw new IllegalStateException("Error calculating compaction priority for table: " + tableId);
   }
 
+  private static Range<Short> calculateUserTableRange(CompactionKind calculationKind,
+      int totalFiles, int maxFilesPerTablet) {
+    if (totalFiles > maxFilesPerTablet && calculationKind == CompactionKind.SYSTEM) {
+      return TABLE_OVER_SIZE;
+    } else if (calculationKind == CompactionKind.SYSTEM) {
+      return USER_TABLE_SYSTEM;
+    } else {
+      return USER_TABLE_USER;
+    }
+  }
+
+  private static Function<Range<Short>,Short> selectPriorityFunction(int totalFiles,
+      int compactingFiles, int maxFilesPerTablet, CompactionKind calculationKind) {
+    if (totalFiles > maxFilesPerTablet && calculationKind == CompactionKind.SYSTEM) {
+      return f -> (short) Math.min(f.getMaximum(),
+          f.getMinimum() + compactingFiles + (totalFiles - maxFilesPerTablet));
+    } else {
+      return f -> (short) Math.min(f.getMaximum(), f.getMinimum() + totalFiles + compactingFiles);
+    }
+  }
 }
