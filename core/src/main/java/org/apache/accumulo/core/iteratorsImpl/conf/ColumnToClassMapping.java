ea6900f497f09ae5ed9d8dee4a0ff943f2f12abf
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-19 11:08:53 -0500

    Your changing file core/src/main/java/org/apache/accumulo/core/iteratorsImpl/conf/ColumnToClassMapping.java

--- core/src/main/java/org/apache/accumulo/core/iteratorsImpl/conf/ColumnToClassMapping.java
+++ core/src/main/java/org/apache/accumulo/core/iteratorsImpl/conf/ColumnToClassMapping.java
@@ -1,21 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 package org.apache.accumulo.core.iteratorsImpl.conf;
 
 import java.io.IOException;
@@ -51,22 +34,29 @@ public class ColumnToClassMapping<K> {
   public ColumnToClassMapping(Map<String,String> objectStrings, Class<? extends K> c,
       String context) throws ReflectiveOperationException, IOException {
     this();
+    initializeMapping(objectStrings, c, context);
+  }
 
+  private void initializeMapping(Map<String,String> objectStrings, Class<? extends K> c,
+      String context) throws ReflectiveOperationException, IOException {
     for (Entry<String,String> entry : objectStrings.entrySet()) {
       String column = entry.getKey();
       String className = entry.getValue();
-
       Pair<Text,Text> pcic = ColumnSet.decodeColumns(column);
+      K inst = createInstance(context, className, c);
 
-      Class<? extends K> clazz = ClassLoaderUtil.loadClass(context, className, c);
-
-      K inst = clazz.getDeclaredConstructor().newInstance();
       if (pcic.getSecond() == null) {
         addObject(pcic.getFirst(), inst);
       } else {
         addObject(pcic.getFirst(), pcic.getSecond(), inst);
       }
     }
+  }
+
+  private K createInstance(String context, String className, Class<? extends K> c)
+      throws ReflectiveOperationException, IOException {
+    Class<? extends K> clazz = ClassLoaderUtil.loadClass(context, className, c);
+    return clazz.getDeclaredConstructor().newInstance();
   }
 
   protected void addObject(Text colf, K obj) {
@@ -78,24 +68,27 @@ public class ColumnToClassMapping<K> {
   }
 
   public K getObject(Key key) {
-    K obj = null;
+    K obj = lookupObjectInColumns(key);
+    if (obj == null) {
+      obj = lookupObjectInColumnFamilies(key);
+    }
+    return obj;
+  }
 
-    // lookup column family and column qualifier
+  private K lookupObjectInColumns(Key key) {
     if (!objectsCol.isEmpty()) {
       lookupCol.set(key);
-      obj = objectsCol.get(lookupCol);
-      if (obj != null) {
-        return obj;
-      }
+      return objectsCol.get(lookupCol);
     }
+    return null;
+  }
 
-    // lookup just column family
+  private K lookupObjectInColumnFamilies(Key key) {
     if (!objectsCF.isEmpty()) {
       lookupCF.set(key);
-      obj = objectsCF.get(lookupCF);
+      return objectsCF.get(lookupCF);
     }
-
-    return obj;
+    return null;
   }
 
   public boolean isEmpty() {
