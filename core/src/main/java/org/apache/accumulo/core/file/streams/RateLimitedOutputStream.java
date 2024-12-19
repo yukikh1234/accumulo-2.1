24cb2a28f46c72a34bff582551aa11ca1ca73fa4
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-19 14:21:19 -0500

    Your changing file core/src/main/java/org/apache/accumulo/core/file/streams/RateLimitedOutputStream.java

--- core/src/main/java/org/apache/accumulo/core/file/streams/RateLimitedOutputStream.java
+++ core/src/main/java/org/apache/accumulo/core/file/streams/RateLimitedOutputStream.java
@@ -1,21 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 package org.apache.accumulo.core.file.streams;
 
 import java.io.DataOutputStream;
@@ -40,18 +23,18 @@ public class RateLimitedOutputStream extends DataOutputStream {
   @Override
   public synchronized void write(int i) throws IOException {
     writeLimiter.acquire(1);
-    out.write(i);
+    super.write(i);
   }
 
   @Override
   public synchronized void write(byte[] buffer, int offset, int length) throws IOException {
     writeLimiter.acquire(length);
-    out.write(buffer, offset, length);
+    super.write(buffer, offset, length);
   }
 
   @Override
   public void close() throws IOException {
-    out.close();
+    super.close();
   }
 
   public long position() {
