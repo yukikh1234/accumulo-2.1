4a3e7746f13ebcba7b2ea27d8d3cd7dcfb9916be
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-19 11:47:30 -0500

    Your changing file core/src/main/java/org/apache/accumulo/core/util/LocalityGroupUtil.java

--- core/src/main/java/org/apache/accumulo/core/util/LocalityGroupUtil.java
+++ core/src/main/java/org/apache/accumulo/core/util/LocalityGroupUtil.java
@@ -1,3 +1,4 @@
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -46,7 +47,6 @@ import org.apache.accumulo.core.data.Range;
 import org.apache.accumulo.core.data.TableId;
 import org.apache.accumulo.core.dataImpl.thrift.TMutation;
 import org.apache.accumulo.core.file.FileSKVIterator;
-import org.apache.accumulo.core.file.rfile.RFile.Reader;
 import org.apache.commons.lang3.mutable.MutableLong;
 import org.apache.hadoop.io.Text;
 import org.slf4j.Logger;
@@ -60,14 +60,6 @@ public class LocalityGroupUtil {
 
   private static final Logger log = LoggerFactory.getLogger(LocalityGroupUtil.class);
 
-  /**
-   * Create a set of families to be passed into the SortedKeyValueIterator seek call from a supplied
-   * set of columns. We are using the immutable set to enable faster comparisons down in the
-   * LocalityGroupIterator.
-   *
-   * @param columns The set of columns
-   * @return An immutable set of columns
-   */
   public static Set<ByteSequence> families(Collection<Column> columns) {
     if (columns.isEmpty()) {
       return Set.of();
@@ -112,6 +104,13 @@ public class LocalityGroupUtil {
   public static Map<String,Set<ByteSequence>> getLocalityGroups(AccumuloConfiguration acuconf)
       throws LocalityGroupConfigurationError {
     Map<String,Set<ByteSequence>> result = new HashMap<>();
+    populateGroups(acuconf, result);
+    validateGroups(result);
+    return result;
+  }
+
+  private static void populateGroups(AccumuloConfiguration acuconf,
+      Map<String,Set<ByteSequence>> result) throws LocalityGroupConfigurationError {
     String[] groups = acuconf.get(Property.TABLE_LOCALITY_GROUPS).split(",");
     for (String group : groups) {
       if (!group.isEmpty()) {
@@ -124,45 +123,43 @@ public class LocalityGroupUtil {
       String value = entry.getValue();
       String prefix = Property.TABLE_LOCALITY_GROUP_PREFIX.getKey();
       if (property.startsWith(prefix)) {
-        // this property configures a locality group, find out which one:
-        String group = property.substring(prefix.length());
-        String[] parts = group.split("\\.");
-        group = parts[0];
-        if (result.containsKey(group) && (parts.length == 1)) {
+        String group = property.substring(prefix.length()).split("\\.")[0];
+        if (result.containsKey(group)) {
           Set<ByteSequence> colFamsSet = decodeColumnFamilies(value);
-          if (!Collections.disjoint(all, colFamsSet)) {
-            colFamsSet.retainAll(all);
-            throw new LocalityGroupConfigurationError("Column families " + colFamsSet + " in group "
-                + group + " is already used by another locality group");
-          }
-
+          checkForOverlap(all, colFamsSet, group);
           all.addAll(colFamsSet);
           result.put(group, colFamsSet);
         }
       }
     }
+  }
 
-    Set<Entry<String,Set<ByteSequence>>> es = result.entrySet();
-    for (Entry<String,Set<ByteSequence>> entry : es) {
+  private static void checkForOverlap(HashSet<ByteSequence> all, Set<ByteSequence> colFamsSet,
+      String group) throws LocalityGroupConfigurationError {
+    if (!Collections.disjoint(all, colFamsSet)) {
+      colFamsSet.retainAll(all);
+      throw new LocalityGroupConfigurationError("Column families " + colFamsSet + " in group "
+          + group + " is already used by another locality group");
+    }
+  }
+
+  private static void validateGroups(Map<String,Set<ByteSequence>> result)
+      throws LocalityGroupConfigurationError {
+    for (Entry<String,Set<ByteSequence>> entry : result.entrySet()) {
       if (entry.getValue().isEmpty()) {
         throw new LocalityGroupConfigurationError(
             "Locality group " + entry.getKey() + " specified but not declared");
       }
     }
-
-    // result.put("", all);
-    return result;
   }
 
   public static Set<ByteSequence> decodeColumnFamilies(String colFams)
       throws LocalityGroupConfigurationError {
     HashSet<ByteSequence> colFamsSet = new HashSet<>();
-
     for (String family : colFams.split(",")) {
       ByteSequence cfbs = decodeColumnFamily(family);
       colFamsSet.add(cfbs);
     }
-
     return colFamsSet;
   }
 
@@ -175,49 +172,45 @@ public class LocalityGroupUtil {
       char c = colFam.charAt(i);
 
       if (c == '\\') {
-        // next char must be 'x' or '\'
-        i++;
-
-        if (i >= colFam.length()) {
-          throw new LocalityGroupConfigurationError("Expected 'x' or '\' after '\'  in " + colFam);
-        }
-
-        char nc = colFam.charAt(i);
-
-        switch (nc) {
-          case '\\':
-            output[pos++] = '\\';
-            break;
-          case 'x':
-            // next two chars must be [0-9][0-9]
-            i++;
-            output[pos++] = (byte) (0xff & Integer.parseInt(colFam.substring(i, i + 2), 16));
-            i++;
-            break;
-          default:
-            throw new LocalityGroupConfigurationError(
-                "Expected 'x' or '\' after '\'  in " + colFam);
-        }
+        pos = handleEscapeSequence(colFam, output, pos, i);
+        i += 2;
       } else {
         output[pos++] = (byte) (0xff & c);
       }
-
     }
 
     return new ArrayByteSequence(output, 0, pos);
+  }
 
+  private static int handleEscapeSequence(String colFam, byte[] output, int pos, int i)
+      throws LocalityGroupConfigurationError {
+    if (++i >= colFam.length()) {
+      throw new LocalityGroupConfigurationError("Expected 'x' or '\\' after '\\' in " + colFam);
+    }
+
+    char nc = colFam.charAt(i);
+
+    if (nc == '\\') {
+      output[pos++] = '\\';
+    } else if (nc == 'x') {
+      if (i + 2 >= colFam.length()) {
+        throw new LocalityGroupConfigurationError("Incomplete hex escape sequence in " + colFam);
+      }
+      output[pos++] = (byte) (0xff & Integer.parseInt(colFam.substring(i + 1, i + 3), 16));
+      i += 2;
+    } else {
+      throw new LocalityGroupConfigurationError("Expected 'x' or '\\' after '\\' in " + colFam);
+    }
+    return pos;
   }
 
   public static String encodeColumnFamilies(Set<Text> colFams) {
     SortedSet<String> ecfs = new TreeSet<>();
-
     StringBuilder sb = new StringBuilder();
-
     for (Text text : colFams) {
       String ecf = encodeColumnFamily(sb, text.getBytes(), text.getLength());
       ecfs.add(ecf);
     }
-
     return Joiner.on(",").join(ecfs);
   }
 
@@ -230,7 +223,6 @@ public class LocalityGroupUtil {
 
   private static String encodeColumnFamily(StringBuilder sb, byte[] ba, int len) {
     sb.setLength(0);
-
     for (int i = 0; i < len; i++) {
       int c = 0xff & ba[i];
       if (c == '\\') {
@@ -241,7 +233,6 @@ public class LocalityGroupUtil {
         sb.append("\\x").append(String.format("%02X", c));
       }
     }
-
     return sb.toString();
   }
 
@@ -315,38 +306,55 @@ public class LocalityGroupUtil {
           int lgid = getLgid(mbs, mutation.getUpdates().get(0));
           partitionedMutations.get(lgid).add(mutation);
         } else {
-          for (int i = 0; i < parts.length; i++) {
-            parts.set(i, null);
-          }
+          partitionMultipleUpdates(mutation, partitionedMutations, parts, mbs);
+        }
+      }
+    }
 
-          int lgcount = 0;
+    private void partitionMultipleUpdates(Mutation mutation,
+        PreAllocatedArray<List<Mutation>> partitionedMutations,
+        PreAllocatedArray<List<ColumnUpdate>> parts, MutableByteSequence mbs) {
+      for (int i = 0; i < parts.length; i++) {
+        parts.set(i, null);
+      }
 
-          for (ColumnUpdate cu : mutation.getUpdates()) {
-            int lgid = getLgid(mbs, cu);
+      int lgcount = 0;
 
-            if (parts.get(lgid) == null) {
-              parts.set(lgid, new ArrayList<>());
-              lgcount++;
-            }
+      for (ColumnUpdate cu : mutation.getUpdates()) {
+        int lgid = getLgid(mbs, cu);
 
-            parts.get(lgid).add(cu);
-          }
+        if (parts.get(lgid) == null) {
+          parts.set(lgid, new ArrayList<>());
+          lgcount++;
+        }
 
-          if (lgcount == 1) {
-            for (int i = 0; i < parts.length; i++) {
-              if (parts.get(i) != null) {
-                partitionedMutations.get(i).add(mutation);
-                break;
-              }
-            }
-          } else {
-            for (int i = 0; i < parts.length; i++) {
-              if (parts.get(i) != null) {
-                partitionedMutations.get(i)
-                    .add(new PartitionedMutation(mutation.getRow(), parts.get(i)));
-              }
-            }
-          }
+        parts.get(lgid).add(cu);
+      }
+
+      if (lgcount == 1) {
+        addSingleGroup(mutation, partitionedMutations, parts);
+      } else {
+        addMultipleGroups(mutation, partitionedMutations, parts);
+      }
+    }
+
+    private void addSingleGroup(Mutation mutation,
+        PreAllocatedArray<List<Mutation>> partitionedMutations,
+        PreAllocatedArray<List<ColumnUpdate>> parts) {
+      for (int i = 0; i < parts.length; i++) {
+        if (parts.get(i) != null) {
+          partitionedMutations.get(i).add(mutation);
+          break;
+        }
+      }
+    }
+
+    private void addMultipleGroups(Mutation mutation,
+        PreAllocatedArray<List<Mutation>> partitionedMutations,
+        PreAllocatedArray<List<ColumnUpdate>> parts) {
+      for (int i = 0; i < parts.length; i++) {
+        if (parts.get(i) != null) {
+          partitionedMutations.get(i).add(new PartitionedMutation(mutation.getRow(), parts.get(i)));
         }
       }
     }
@@ -361,25 +369,12 @@ public class LocalityGroupUtil {
     }
   }
 
-  /**
-   * This method created to help seek an rfile for a locality group obtained from
-   * {@link Reader#getLocalityGroupCF()}. This method can possibly return an empty list for the
-   * default locality group. When this happens the default locality group needs to be seeked
-   * differently. This method helps do that.
-   *
-   * <p>
-   * For the default locality group will seek using the families of all other locality groups
-   * non-inclusive.
-   *
-   * @see Reader#getLocalityGroupCF()
-   */
   public static void seek(FileSKVIterator reader, Range range, String lgName,
       Map<String,ArrayList<ByteSequence>> localityGroupCF) throws IOException {
 
     Collection<ByteSequence> families;
     boolean inclusive;
     if (lgName == null) {
-      // this is the default locality group, create a set of all families not in the default group
       Set<ByteSequence> nonDefaultFamilies = new HashSet<>();
       localityGroupCF.forEach((k, v) -> {
         if (k != null) {
@@ -400,16 +395,19 @@ public class LocalityGroupUtil {
   public static void ensureNonOverlappingGroups(Map<String,Set<Text>> groups) {
     HashSet<Text> all = new HashSet<>();
     for (Entry<String,Set<Text>> entry : groups.entrySet()) {
-      if (!Collections.disjoint(all, entry.getValue())) {
-        throw new IllegalArgumentException(
-            "Group " + entry.getKey() + " overlaps with another group");
-      }
-
-      if (entry.getValue().isEmpty()) {
-        throw new IllegalArgumentException("Group " + entry.getKey() + " is empty");
-      }
-
+      validateGroup(entry, all);
       all.addAll(entry.getValue());
+    }
+  }
+
+  private static void validateGroup(Entry<String,Set<Text>> entry, HashSet<Text> all) {
+    if (!Collections.disjoint(all, entry.getValue())) {
+      throw new IllegalArgumentException(
+          "Group " + entry.getKey() + " overlaps with another group");
+    }
+
+    if (entry.getValue().isEmpty()) {
+      throw new IllegalArgumentException("Group " + entry.getKey() + " is empty");
     }
   }
 }
