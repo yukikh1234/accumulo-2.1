4f9e4cdd5ffbf86c6ac0f933311a09bfbb0e94a6
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-19 15:28:43 -0500

    Your changing file core/src/main/java/org/apache/accumulo/core/file/rfile/bcfile/SimpleBufferedOutputStream.java

--- core/src/main/java/org/apache/accumulo/core/file/rfile/bcfile/SimpleBufferedOutputStream.java
+++ core/src/main/java/org/apache/accumulo/core/file/rfile/bcfile/SimpleBufferedOutputStream.java
@@ -1,21 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 package org.apache.accumulo.core.file.rfile.bcfile;
 
 import java.io.FilterOutputStream;
@@ -54,13 +37,21 @@ class SimpleBufferedOutputStream extends FilterOutputStream {
   @Override
   public void write(byte[] b, int off, int len) throws IOException {
     if (len >= buf.length) {
-      flushBuffer();
-      out.write(b, off, len);
+      handleLargeWrite(b, off, len);
       return;
     }
     if (len > buf.length - count) {
       flushBuffer();
     }
+    bufferWrite(b, off, len);
+  }
+
+  private void handleLargeWrite(byte[] b, int off, int len) throws IOException {
+    flushBuffer();
+    out.write(b, off, len);
+  }
+
+  private void bufferWrite(byte[] b, int off, int len) {
     System.arraycopy(b, off, buf, count, len);
     count += len;
   }
