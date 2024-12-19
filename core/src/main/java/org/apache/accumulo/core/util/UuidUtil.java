9edb74befbc600b798ee8cf5b6da6d107e940afd
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-19 12:50:52 -0500

    Your changing file core/src/main/java/org/apache/accumulo/core/util/UuidUtil.java

--- core/src/main/java/org/apache/accumulo/core/util/UuidUtil.java
+++ core/src/main/java/org/apache/accumulo/core/util/UuidUtil.java
@@ -1,24 +1,8 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 package org.apache.accumulo.core.util;
 
 public class UuidUtil {
+
   /**
    * A fast method for verifying a suffix of a string looks like a uuid.
    *
@@ -30,17 +14,18 @@ public class UuidUtil {
       return false;
     }
     for (int i = 0; i < 36; i++) {
-      var c = uuid.charAt(i + offset);
-      if (i == 8 || i == 13 || i == 18 || i == 23) {
-        if (c != '-') {
-          // expect '-' char at above positions, did not see it
-          return false;
-        }
-      } else if (c < '0' || (c > '9' && c < 'A') || (c > 'F' && c < 'a') || c > 'f') {
-        // expected hex at all other positions, did not see hex chars
+      char c = uuid.charAt(i + offset);
+      if (!isValidUUIDChar(c, i)) {
         return false;
       }
     }
     return true;
+  }
+
+  private static boolean isValidUUIDChar(char c, int position) {
+    if (position == 8 || position == 13 || position == 18 || position == 23) {
+      return c == '-';
+    }
+    return (c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f');
   }
 }
