eafb4dcfb8a57244f0d399839a0c2837077919f9
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-19 12:15:10 -0500

    Your changing file core/src/main/java/org/apache/accumulo/core/util/PreAllocatedArray.java

--- core/src/main/java/org/apache/accumulo/core/util/PreAllocatedArray.java
+++ core/src/main/java/org/apache/accumulo/core/util/PreAllocatedArray.java
@@ -1,27 +1,9 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 package org.apache.accumulo.core.util;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
-
-import com.google.common.collect.Iterators;
 
 /**
  * An {@link ArrayList} implementation that represents a type-safe pre-allocated array. This should
@@ -37,29 +19,26 @@ public class PreAllocatedArray<T> implements Iterable<T> {
    * Creates an instance of the given capacity, with all elements initialized to null
    */
   public PreAllocatedArray(final int capacity) {
-    length = capacity;
-    internal = new ArrayList<>(capacity);
-    for (int i = 0; i < capacity; i++) {
-      internal.add(null);
-    }
+    this.length = capacity;
+    this.internal = new ArrayList<>(Collections.nCopies(capacity, null));
   }
 
   /**
    * Set the element at the specified index, and return the old value.
    */
   public T set(final int index, final T element) {
-    return internal.set(index, element);
+    return this.internal.set(index, element);
   }
 
   /**
    * Get the item stored at the specified index.
    */
   public T get(final int index) {
-    return internal.get(index);
+    return this.internal.get(index);
   }
 
   @Override
   public Iterator<T> iterator() {
-    return Iterators.unmodifiableIterator(internal.iterator());
+    return Collections.unmodifiableList(this.internal).iterator();
   }
 }
