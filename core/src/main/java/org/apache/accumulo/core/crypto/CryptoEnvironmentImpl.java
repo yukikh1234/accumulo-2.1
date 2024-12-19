96050c537a17f06c06a0e6e8cddfcee8f8404812
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-19 09:47:44 -0500

    Your changing file core/src/main/java/org/apache/accumulo/core/crypto/CryptoEnvironmentImpl.java

--- core/src/main/java/org/apache/accumulo/core/crypto/CryptoEnvironmentImpl.java
+++ core/src/main/java/org/apache/accumulo/core/crypto/CryptoEnvironmentImpl.java
@@ -1,21 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 package org.apache.accumulo.core.crypto;
 
 import java.util.Objects;
@@ -40,15 +23,13 @@ public class CryptoEnvironmentImpl implements CryptoEnvironment {
    */
   public CryptoEnvironmentImpl(Scope scope, @Nullable TableId tableId,
       @Nullable byte[] decryptionParams) {
-    this.scope = Objects.requireNonNull(scope);
+    this.scope = Objects.requireNonNull(scope, "Scope cannot be null");
     this.tableId = tableId;
     this.decryptionParams = decryptionParams;
   }
 
   public CryptoEnvironmentImpl(Scope scope) {
-    this.scope = scope;
-    this.tableId = null;
-    this.decryptionParams = null;
+    this(scope, null, null);
   }
 
   @Override
@@ -68,12 +49,14 @@ public class CryptoEnvironmentImpl implements CryptoEnvironment {
 
   @Override
   public String toString() {
-    String str = scope + " tableId=" + tableId + " decryptParams.length=";
+    StringBuilder strBuilder = new StringBuilder();
+    strBuilder.append("Scope: ").append(scope).append(", TableId: ").append(tableId)
+        .append(", DecryptParams.length: ");
     if (decryptionParams == null) {
-      str += 0;
+      strBuilder.append(0);
     } else {
-      str += decryptionParams.length;
+      strBuilder.append(decryptionParams.length);
     }
-    return str;
+    return strBuilder.toString();
   }
 }
