0fdf172976923d1b492b463c542227567f36d57a
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-19 12:26:31 -0500

    Your changing file core/src/main/java/org/apache/accumulo/core/util/UtilWaitThread.java

--- core/src/main/java/org/apache/accumulo/core/util/UtilWaitThread.java
+++ core/src/main/java/org/apache/accumulo/core/util/UtilWaitThread.java
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 package org.apache.accumulo.core.util;
 
 import static java.util.concurrent.TimeUnit.NANOSECONDS;
@@ -29,6 +30,10 @@ public class UtilWaitThread {
   private static final Logger log = LoggerFactory.getLogger(UtilWaitThread.class);
 
   public static void sleep(long millis) {
+    sleepWithLogging(millis);
+  }
+
+  private static void sleepWithLogging(long millis) {
     try {
       Thread.sleep(millis);
     } catch (InterruptedException e) {
@@ -36,35 +41,26 @@ public class UtilWaitThread {
     }
   }
 
-  /**
-   * Copied from Guava release 23. The Uniterruptibles class was annotated as Beta by Google,
-   * therefore unstable to use. The following javadoc was copied from
-   * com.google.common.util.concurrent.Uninterruptibles:
-   *
-   * Utilities for treating interruptible operations as uninterruptible. In all cases, if a thread
-   * is interrupted during such a call, the call continues to block until the result is available or
-   * the timeout elapses, and only then re-interrupts the thread.
-   *
-   */
   public static void sleepUninterruptibly(long sleepFor, TimeUnit unit) {
     boolean interrupted = false;
-    try {
-      long remainingNanos = unit.toNanos(sleepFor);
-      long end = System.nanoTime() + remainingNanos;
-      while (true) {
-        try {
-          // TimeUnit.sleep() treats negative timeouts just like zero.
-          NANOSECONDS.sleep(remainingNanos);
-          return;
-        } catch (InterruptedException e) {
-          interrupted = true;
-          remainingNanos = end - System.nanoTime();
+    long remainingNanos = unit.toNanos(sleepFor);
+    long end = System.nanoTime() + remainingNanos;
+    while (true) {
+      try {
+        sleepForRemainingNanos(remainingNanos);
+        return;
+      } catch (InterruptedException e) {
+        interrupted = true;
+        remainingNanos = end - System.nanoTime();
+      } finally {
+        if (interrupted) {
+          Thread.currentThread().interrupt();
         }
       }
-    } finally {
-      if (interrupted) {
-        Thread.currentThread().interrupt();
-      }
     }
+  }
+
+  private static void sleepForRemainingNanos(long remainingNanos) throws InterruptedException {
+    NANOSECONDS.sleep(remainingNanos);
   }
 }
