b4f83d7f9fea95c3bffcfe616110994c024752f6
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-19 11:49:08 -0500

    Your changing file core/src/main/java/org/apache/accumulo/core/util/Help.java

--- core/src/main/java/org/apache/accumulo/core/util/Help.java
+++ core/src/main/java/org/apache/accumulo/core/util/Help.java
@@ -16,6 +16,25 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.accumulo.core.util;
 
 import org.apache.accumulo.start.Main;
@@ -25,19 +44,24 @@ import com.google.auto.service.AutoService;
 
 @AutoService(KeywordExecutable.class)
 public class Help implements KeywordExecutable {
+
+  private static final String KEYWORD = "help";
+  private static final String DESCRIPTION = "Prints usage";
+  private static final UsageGroup USAGE_GROUP = UsageGroup.CORE;
+
   @Override
   public String keyword() {
-    return "help";
+    return KEYWORD;
   }
 
   @Override
   public UsageGroup usageGroup() {
-    return UsageGroup.CORE;
+    return USAGE_GROUP;
   }
 
   @Override
   public String description() {
-    return "Prints usage";
+    return DESCRIPTION;
   }
 
   @Override
