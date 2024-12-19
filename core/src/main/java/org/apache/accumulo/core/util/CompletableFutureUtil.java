3905bf86b2851b9df1c43e4c52502ad9a0cf6ea8
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-19 11:50:20 -0500

    Your changing file core/src/main/java/org/apache/accumulo/core/util/CompletableFutureUtil.java

--- core/src/main/java/org/apache/accumulo/core/util/CompletableFutureUtil.java
+++ core/src/main/java/org/apache/accumulo/core/util/CompletableFutureUtil.java
@@ -1,21 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 package org.apache.accumulo.core.util;
 
 import java.util.ArrayList;
@@ -28,41 +11,49 @@ import java.util.function.Supplier;
 
 public class CompletableFutureUtil {
 
-  // create a binary tree of completable future operations, where each node in the tree merges the
+  // Create a binary tree of completable future operations, where each node in the tree merges the
   // results of their children when complete
   public static <T> CompletableFuture<T> merge(List<CompletableFuture<T>> futures,
       BiFunction<T,T,T> mergeFunc, Supplier<T> nothing) {
     if (futures.isEmpty()) {
       return CompletableFuture.completedFuture(nothing.get());
     }
+    // Break down the merge logic into smaller methods for readability
     while (futures.size() > 1) {
-      ArrayList<CompletableFuture<T>> mergedFutures = new ArrayList<>(futures.size() / 2);
-      for (int i = 0; i < futures.size(); i += 2) {
-        if (i + 1 == futures.size()) {
-          mergedFutures.add(futures.get(i));
-        } else {
-          mergedFutures.add(futures.get(i).thenCombine(futures.get(i + 1), mergeFunc));
-        }
-      }
-
-      futures = mergedFutures;
+      futures = mergeFutures(futures, mergeFunc);
     }
-
     return futures.get(0);
+  }
+
+  // Helper method to merge futures in pairs
+  private static <T> List<CompletableFuture<T>> mergeFutures(List<CompletableFuture<T>> futures,
+      BiFunction<T,T,T> mergeFunc) {
+    ArrayList<CompletableFuture<T>> mergedFutures = new ArrayList<>(futures.size() / 2);
+    for (int i = 0; i < futures.size(); i += 2) {
+      mergedFutures.add(mergePair(futures, i, mergeFunc));
+    }
+    return mergedFutures;
+  }
+
+  // Helper method to merge a pair of futures
+  private static <T> CompletableFuture<T> mergePair(List<CompletableFuture<T>> futures, int i,
+      BiFunction<T,T,T> mergeFunc) {
+    if (i + 1 == futures.size()) {
+      return futures.get(i);
+    } else {
+      return futures.get(i).thenCombine(futures.get(i + 1), mergeFunc);
+    }
   }
 
   /**
    * Iterate some function until a given condition is met.
    *
-   * The step function should always return an asynchronous {@code
-   * CompletableFuture} in order to avoid stack overflows.
+   * The step function should always return an asynchronous {@code CompletableFuture} in order to
+   * avoid stack overflows.
    */
   public static <T> CompletableFuture<T> iterateUntil(Function<T,CompletableFuture<T>> step,
       Predicate<T> isDone, T init) {
-    // We'd like to use a lambda here, but lambdas don't have
-    // `this`, so we would have to use some clumsy indirection to
-    // achieve self-reference.
-    Function<T,CompletableFuture<T>> go = new Function<>() {
+    Function<T,CompletableFuture<T>> recursiveFunction = new Function<>() {
       @Override
       public CompletableFuture<T> apply(T x) {
         if (isDone.test(x)) {
@@ -71,6 +62,6 @@ public class CompletableFutureUtil {
         return step.apply(x).thenCompose(this);
       }
     };
-    return CompletableFuture.completedFuture(init).thenCompose(go);
+    return CompletableFuture.completedFuture(init).thenCompose(recursiveFunction);
   }
 }
