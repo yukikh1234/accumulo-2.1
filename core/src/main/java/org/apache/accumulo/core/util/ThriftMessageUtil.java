a0d1db2b16e20e973b2356173aad3bf740d8bf57
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-19 12:21:22 -0500

    Your changing file core/src/main/java/org/apache/accumulo/core/util/ThriftMessageUtil.java

--- core/src/main/java/org/apache/accumulo/core/util/ThriftMessageUtil.java
+++ core/src/main/java/org/apache/accumulo/core/util/ThriftMessageUtil.java
@@ -1,21 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 package org.apache.accumulo.core.util;
 
 import static java.util.Objects.requireNonNull;
@@ -38,13 +21,10 @@ import org.apache.thrift.transport.TTransportException;
 public class ThriftMessageUtil {
 
   private final int initialCapacity;
-
   private final TMemoryInputTransport inputTransport;
   private final TCompactProtocol inputProtocol;
 
   public ThriftMessageUtil() throws IOException {
-    // TODO does this make sense? better to push this down to the serialize method (accept the
-    // transport as an argument)?
     this.initialCapacity = 64;
     try {
       this.inputTransport = new TMemoryInputTransport();
@@ -63,8 +43,7 @@ public class ThriftMessageUtil {
    */
   public ByteBuffer serialize(TBase<?,?> msg) throws IOException {
     requireNonNull(msg);
-    try {
-      TMemoryBuffer transport = new TMemoryBuffer(initialCapacity);
+    try (TMemoryBuffer transport = new TMemoryBuffer(initialCapacity)) {
       TProtocol protocol = new TCompactProtocol(transport);
       msg.write(protocol);
       return ByteBuffer.wrap(transport.getArray(), 0, transport.length());
@@ -93,12 +72,20 @@ public class ThriftMessageUtil {
   public <T extends TBase<?,?>> T deserialize(byte[] serialized, int offset, int length, T instance)
       throws IOException {
     requireNonNull(instance);
+    resetTransport(serialized, offset, length);
+    readInstance(instance);
+    return instance;
+  }
+
+  private void resetTransport(byte[] serialized, int offset, int length) {
     inputTransport.reset(serialized, offset, length);
+  }
+
+  private <T extends TBase<?,?>> void readInstance(T instance) throws IOException {
     try {
       instance.read(inputProtocol);
     } catch (TException e) {
       throw new IOException(e);
     }
-    return instance;
   }
 }
